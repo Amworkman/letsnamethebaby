aad3917bfce9f0485a83d0c58d9ae9b850968946
Adam Workman <adammworkman25@gmail.com>
2020-10-27 19:48:42 -0400

    add through association

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,5 +1,5 @@
 class Parent < ApplicationRecord
-    belong_to :team
+    belongs_to :team
     has_many :babies
-    has_many :baby_names through: :babies
+    has_many :baby_names, through: :babies
 end
