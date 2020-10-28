629e7923e9f3c357f169960dcd72ab2a67b5a5f7
Adam Workman <adammworkman25@gmail.com>
2020-10-28 18:08:05 -0400

    add associations

--- app/models/baby_name.rb
+++ app/models/baby_name.rb
@@ -1,4 +1,4 @@
 class BabyName < ApplicationRecord
-    has_many :babies
-    has_many :parents, through: :babies
+    has_many :parents_baby_names
+    has_many :parents, through: :parents_baby_names
 end
