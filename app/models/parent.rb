917385e647745dc17a827d05608cff2cea896426
Adam Workman <adammworkman25@gmail.com>
2020-10-28 18:09:14 -0400

    edit associations, add custom writer

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,5 +1,17 @@
-class Parent < ApplicationRecord
+class Parent < ApplicationRecord    
     belongs_to :team
     has_many :babies
-    has_many :baby_names, through: :babies
+    has_many :parents_baby_names
+    has_many :baby_names, through: :parents_baby_names
+   
+    def baby_names_attributes=(baby_name_attributes)
+        baby_name_attributes.values.each do |baby_name_attribute|
+            baby_name = BabyName.find_or_create_by(baby_name_attribute)
+            #self.baby_names.find_or_create_by(baby_name.name)
+            unless self.baby_names.exists?(baby_name.id) 
+                self.baby_names << baby_name
+            end
+        end
+    end
+
 end
