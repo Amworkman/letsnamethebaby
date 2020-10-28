28238e88e6ec4186e5a6c010da45bac0db6f39ac
Adam Workman <adammworkman25@gmail.com>
2020-10-28 18:06:50 -0400

    edit show update

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -1,11 +1,41 @@
 class ParentsController < ApplicationController
 
-    def index
-        @parents = Parent.all
+    def index 
+        @team = Team.find_by_id(params[:team_id])       
+        @parents = Parent.where(:team_id => params[:team_id])        
     end
     
     def show
         @parent = Parent.find_by_id(params[:id])
+        @team = @parent.team
+        @baby_names = BabyName.all
+
+        if @parent.babies.length == 2
+            20.times {@baby_name = @parent.baby_names.build}
+        elsif @parent.babies.length == 1
+            10.times {@baby_name = @parent.baby_names.build}\
+        else 
+            raise errors.full_messages
+        end
     end
-    
+
+    def update 
+        @team = @parent.team
+        @baby_names = BabyName.all
+        @parent = Parent.find_by_id(params[:id]) 
+        @parent.parents_baby_names.destroy_all
+        @parent.update(parent_params)
+        if @parent.save
+            render :show
+        else
+            render :index
+            #with errors
+        end
+    end
+
+    private
+
+    def parent_params
+        params.require(:parent).permit( :name, :roll, baby_names_attributes: [:name])
+    end
 end
