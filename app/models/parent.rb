90426ed4c42c3beb7e89a829824e15fa9f012050
Adam Workman <adammworkman25@gmail.com>
2020-10-31 18:04:59 -0400

    update associations

--- app/models/parent.rb
+++ app/models/parent.rb
@@ -1,9 +1,17 @@
 class Parent < ApplicationRecord    
     belongs_to :team
-    has_many :babies
+    belongs_to :baby
     has_many :parents_baby_names
     has_many :baby_names, through: :parents_baby_names
    
+    def add_baby_name_to_list(parent, baby_name)
+        if parent.baby_names.length < 10 
+            parent.baby_names << baby_name
+        else
+            raise "Your list is full"
+        end
+    end
+    
     def baby_names_attributes=(baby_name_attributes)
         baby_name_attributes.values.each do |baby_name_attribute|
             baby_name = BabyName.find_or_create_by(baby_name_attribute)
