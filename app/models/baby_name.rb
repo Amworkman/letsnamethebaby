7988b4f927cb534880ad723f274a65e6a48d48a0
Adam Workman <adammworkman25@gmail.com>
2020-10-31 18:04:15 -0400

    add add_baby_name_to_list method

--- app/models/baby_name.rb
+++ app/models/baby_name.rb
@@ -1,4 +1,13 @@
 class BabyName < ApplicationRecord
     has_many :parents_baby_names
     has_many :parents, through: :parents_baby_names
+    
+    def add_baby_name_to_list(parent, baby_name)
+        if parent.baby_names.length < 10 
+            parent.baby_names << baby_name
+        else
+            raise "Your list is full"
+        end
+    end
+
 end
