c585ee4eb902e41f54125b0f6936d7ac2959cdc9
Adam Workman <adammworkman25@gmail.com>
2020-10-31 18:02:50 -0400

    refactored index, show. add set_params

--- app/controllers/parents_controller.rb
+++ app/controllers/parents_controller.rb
@@ -1,35 +1,33 @@
 class ParentsController < ApplicationController
-
+    before_action :set_parent, only: [:index, :show, :update]
     def index 
         @team = Team.find_by_id(params[:team_id])       
         @parents = Parent.where(:team_id => params[:team_id])        
     end
     
     def show
-        @parent = Parent.find_by_id(params[:id])
         @team = @parent.team
         @baby_names = BabyName.all
 
-        if @parent.babies.length == 2
-            20.times {@baby_name = @parent.baby_names.build}
-        elsif @parent.babies.length == 1
-            10.times {@baby_name = @parent.baby_names.build}\
-        else 
-            raise errors.full_messages
-        end
+        if params[:baby_name_id]
+            @baby_name = BabyName.find_by_id(params[:baby_name_id])
+            @parent.add_baby_name_to_list(@parent, @baby_name)
+        end 
+
+           10.times {@baby_name = @parent.baby_names.build}  
     end
 
     def update 
-        @team = @parent.team
-        @baby_names = BabyName.all
-        @parent = Parent.find_by_id(params[:id]) 
-        @parent.parents_baby_names.destroy_all
-        @parent.update(parent_params)
+            @baby_names = BabyName.all 
+            @parent.parents_baby_names.destroy_all
+            @parent.update(parent_params)
+            @team = @parent.team
+        
         if @parent.save
             render :show
         else
             render :index
-            #with errors
+            raise errors.full_messages
         end
     end
 
@@ -37,5 +35,9 @@ class ParentsController < ApplicationController
 
     def parent_params
         params.require(:parent).permit( :name, :roll, baby_names_attributes: [:name])
+    end
+
+    def set_parent 
+        @parent = Parent.find_by_id(params[:id]) 
     end
 end
