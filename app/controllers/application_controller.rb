cda39e2ced08852591cca86647a32550d09a499e
Adam Workman <adammworkman25@gmail.com>
2020-11-02 22:41:10 -0500

    current_team, current_parent, team_logged_in?, parrent_loggedin?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    helper_method :team_logged_in?
+
+    def current_team 
+        @team ||= Team.find_by_id(session[:team_id])
+    end
+
+    def current_parent
+        @parent ||= Parent.find_by_id(session[:parent_id])
+    end
+
+    def team_logged_in?
+        !current_team.nil?
+    end
+
+    def parent_logged_in?
+        !current_parent.nil?
+    end
+
+    # def wrong_page 
+    #    render "/wrong_page"
+    # end
 end
