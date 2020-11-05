fa2ca2c5042cd2e3bcb93ba889a4c23635fce632
Adam Workman <adammworkman25@gmail.com>
2020-11-05 14:27:14 -0500

    add helper mothods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     helper_method :team_logged_in?
+    helper_method :parent_logged_in?
+    helper_method :current_team
+    helper_method :current_parent
+    helper_method :baby_gender
+    helper_method :boy_names
+    helper_method :girl_names
+    helper_method :all_names
 
     def current_team 
         @team ||= Team.find_by_id(session[:team_id])
@@ -16,6 +23,34 @@ class ApplicationController < ActionController::Base
 
     def parent_logged_in?
         !current_parent.nil?
+    end
+
+    def baby_gender
+        if team_logged_in?
+            if current_team.babies != []
+                current_team.babies.first.gender
+            else
+                "Baby"
+            end
+        else
+            "Baby"
+        end
+    end
+
+    def boy_names
+        BabyName.all.select do |baby_name|
+            baby_name.gender != "Female"
+        end
+    end
+
+    def girl_names
+        BabyName.all.select do |baby_name|
+            baby_name.gender != "Male"
+        end
+    end
+
+    def all_names
+        BabyName.all
     end
 
     # def wrong_page 
