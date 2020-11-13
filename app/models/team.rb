0be5d4ffe219aac93edbdd04e43670b2fbe123be
Adam Workman <adammworkman25@gmail.com>
2020-11-13 15:27:08 -0500

    add methods

--- app/models/team.rb
+++ app/models/team.rb
@@ -15,6 +15,28 @@ class Team < ApplicationRecord
         end
     end
 
+    def reset_and_update_team
+        self.parents.clear
+        self.babies.clear
+
+        self.update(team_params)
+             
+        @baby = current_team.babies.build(baby_params)
+        current_team.parents.each { |parent| @baby.parents << parent }
+    end
+
+    def common_names
+        @parent_one_babies = self.parents.first.baby_names
+        @parent_two_babies = self.parents.last.baby_names
+
+        @baby_names = self.baby_names_arr_dupes(@parent_one_babies, @parent_two_babies)
+    end
+
+    def set_name
+        self.parents.first.name = self.parent_name
+        self.save
+    end
+
     def baby_names_arr_dupes(names_one, names_two)
         names_one & names_two
     end
