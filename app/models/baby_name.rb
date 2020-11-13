94413a8d4321b8728886f27fbe6d796848959b3d
Adam Workman <adammworkman25@gmail.com>
2020-11-13 15:23:16 -0500

    delete redundant method

--- app/models/baby_name.rb
+++ app/models/baby_name.rb
@@ -2,15 +2,5 @@ class BabyName < ApplicationRecord
     has_many :parents_baby_names
     has_many :parents, through: :parents_baby_names
     validates :name, presence: true 
-    validates :name, uniqueness: true
-    
-    def add_baby_name_to_list(parent, baby_name)
-        if parent.baby_names.length < 10 
-            parent.baby_names << baby_name
-        else
-            flash[:alert] = "Your list is full"
-        end
-    end
-
-    
+    validates :name, uniqueness: true    
 end
