dc4148fd1685e7ba65a14f32521c571db7502662
Adam Workman <adammworkman25@gmail.com>
2020-11-13 13:53:17 -0500

    add validations for name

--- app/models/baby_name.rb
+++ app/models/baby_name.rb
@@ -1,12 +1,14 @@
 class BabyName < ApplicationRecord
     has_many :parents_baby_names
     has_many :parents, through: :parents_baby_names
+    validates :name, presence: true 
+    validates :name, uniqueness: true
     
     def add_baby_name_to_list(parent, baby_name)
         if parent.baby_names.length < 10 
             parent.baby_names << baby_name
         else
-            raise "Your list is full"
+            flash[:alert] = "Your list is full"
         end
     end
 
